f9939efba6fbed76a601a26d581da62d428e5e4d
MR-5OBOT <yassir7m7@gmail.com>
2025-04-25 21:38:48 +0100

    remove a print() left in teh debuginf process lol

--- helpers/stats.py
+++ helpers/stats.py
@@ -225,7 +225,6 @@ def consecutive_losses(df: pd.DataFrame) -> int:
         if outcome == "LOSS":
             current_streak += 1
             max_streak = max(max_streak, current_streak)
-            print(max_streak)
         else:
             current_streak = 0
     return max_streak
