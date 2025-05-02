4a1c6bdb790ae6617de4afc05cf24b99fb47aaba
MR-5OBOT <yassir7m7@gmail.com>
2025-05-02 07:02:08 +0100

    .

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
@@ -38,9 +39,33 @@ def outcome_by_day(df):
 def pl_distribution(pl):
     plt.style.use("dark_background")
     fig, ax = plt.subplots(figsize=(8, 6))
-    sns.histplot(pl, bins=10, kde=True, ax=ax, edgecolor="black", linewidth=2)
+    sns.histplot(pl, bins=10, kde=True, ax=ax, edgecolor="black", linewidth=1.5)
     ax.set_title("P/L Distribution")
     ax.set_xlabel("Profit/Loss (%)")
+    fig.tight_layout()
+    plt.show()
+    return fig
+
+
+def risk_vs_reward_scatter(df, risk, pl):
+    plt.style.use("dark_background")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.scatterplot(
+        x=risk,
+        y=pl,
+        hue=df["outcome"],
+        # palette="coolwarm",
+        palette={
+            "WIN": "#395202",
+            "LOSS": "#C05478",
+            "BE": "#333333",
+        },
+        ax=ax,
+    )
+    ax.set_title("Risk vs Reward")
+    ax.set_xlabel("Risk (%)")
+    ax.set_ylabel("Profit/Loss (%)")
+    ax.legend()
     fig.tight_layout()
     plt.show()
     return fig
@@ -51,5 +76,8 @@ if __name__ == "__main__":
     url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQL7L-HMzezpuFCDOuS0wdUm81zbX4iVOokaFUGonVR1XkhS6CeDl1gHUrW4U0Le4zihfpqSDphTu4I/pub?gid=212787870&single=true&output=csv"
     df = pd.read_csv(url)
     # outcome_by_day(df)
-    pl = pl_raw(df)
-    pl_distribution(pl)
+    # pl = pl_raw(df)
+    # risk = risk_raw(df)
+    pl = np.random.uniform(-1.1, 3.1, 60)
+    risk = np.random.uniform(0.5, 1.10, 60)
+    risk_vs_reward_scatter(df, risk, pl)
