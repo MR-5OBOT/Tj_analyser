a85ea1c8fd90936171d0a17f2321759a68af8a23
MR-5OBOT <yassir7m7@gmail.com>
2025-05-06 07:48:32 +0100

    try making this streamlit version good

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ from helpers.stats import pl_raw, stats_table
 from helpers.utils import df_check
 
 
-def visualizations(df: pd.DataFrame):
+def visualizations(pl: pd.Series):
     with col1:
         fig = pl_curve(df, pl)
         st.pyplot(fig)
@@ -32,10 +32,15 @@ def visualizations(df: pd.DataFrame):
         st.pyplot(fig)
 
 
-st.set_page_config(layout="wide", page_title="Trading Journal Dashboard")
-input_method = st.radio("Choose input method", ["URL", "Upload"])
+def run_process(df: pd.DataFrame):
+    pl = pl_raw(df)
+    st.dataframe(df)
+    visualizations(pl)
 
-df = None  # df initialization
+
+st.set_page_config(layout="wide", page_title="Trading Journal Dashboard")
+input_method = st.radio("Choose input method", ["URL", "Upload"], horizontal=True)
+col1, col2 = st.columns(2, gap="small")
 
 # Handle URL input
 if input_method == "URL":
@@ -43,27 +48,17 @@ if input_method == "URL":
     if csv_url:
         try:
             df = pd.read_csv(csv_url, usecols=range(8))
-            # st.write("DataFrame with first 8 columns:")
-            st.dataframe(df)
-            visualizations(df)
+            run_process(df)
         except Exception as e:
             st.error(f"Error reading CSV: {e}")
-
 # Handle file upload
 elif input_method == "Upload":
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
     if uploaded_file:
         try:
             df = pd.read_csv(uploaded_file, usecols=range(8))
-            # st.write("DataFrame with first 8 columns:")
-            st.dataframe(df)
-            visualizations(df)
+            run_process(df)
         except Exception as e:
             st.error(f"Error reading CSV: {e}")
 else:
     st.write("Please upload a CSV or Excel file to visualize your trading journal.")
-
-
-if __name__ == "__main__":
-    col1, col2 = st.columns(2, gap="small")
-    pl = pl_raw(df)
